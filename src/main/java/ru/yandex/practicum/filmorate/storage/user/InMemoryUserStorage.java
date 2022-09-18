c66c3e83fc37a88240b5ab0fbaf7121a9fb6fb26
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-09-18 12:34:49 +0400

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -0,0 +1,100 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.controller.ValidateException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Component
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+    private Long newId = 1L;
+
+    private Long getNewId() {
+        return newId++;
+    }
+
+    public Optional<User> createUser(User user) {
+
+            if (user.getName().equals("") || user.getName() == null) {
+                user.setName(user.getLogin());
+            }
+            if (!user.getEmail().contains("@") || user.getEmail().isBlank() || user.getEmail() == null) {
+                throw new ValidateException("неправильный формат email или пустой email");
+            }
+            if (user.getLogin().contains(" ") || user.getLogin().isBlank() || user.getLogin() == null) {
+                throw new ValidateException("пустой логин или содержит пробелы");
+            }
+            if (user.getBirthday().isAfter(LocalDate.now())) {
+                throw new ValidateException("дата рождения указывает на будущее время");
+            }
+
+
+        user.setId(getNewId());
+        users.put(user.getId(), user);
+        return Optional.of(user);
+    }
+
+    public Optional<User> updateUser(User user) {
+        if (user.getId() <= 0 || user.getId() == null) {
+            throw new ValidateException("id должен быть > 0");
+        }
+        users.put(user.getId(), user);
+        return Optional.of(user);
+    }
+
+    public List<User> getAll() {
+        List<User> list = new ArrayList<>();
+        for (Long id : users.keySet()) {
+            list.add(users.get(id));
+        }
+        return list;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        users.remove(id);
+    }
+
+    public void deleteAll() {
+        users.clear();
+    }
+
+    public Optional<User> getById(Long id) {
+        if (users.get(id) == null) {
+            throw new ValidateException("user not found");
+        }
+        return Optional.of(users.get(id));
+    }
+
+    public void addFriend(Long id, Long idFriend) {
+    }
+
+    @Override
+    public void deleteFriend(Long id, Long idFriend) {
+
+    }
+
+    @Override
+    public List<User> getFriends(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getCommonFriends(Long id, Long idFriend) {
+        return null;
+    }
+
+    public void deleteFriend(Long id) {
+    }
+
+    public List<User> getFriends() {
+        return null;
+    }
+
+    public List<User> getCommonFriends() {
+        return null;
+    }
+}
